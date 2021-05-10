5a170c1c779c2655c169381fe99cb96f64e22191
cndepalma <77604548+cndepalma@users.noreply.github.com>
2021-05-09 20:10:36 -0400

    Add files via upload

---
+++ Alarm Clock/Alarm Clock/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Alarm_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnSnooze = new System.Windows.Forms.Button();
+            this.btnActivate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(22, 19);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(284, 108);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "00:00";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "h:mm tt";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(133, 130);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(137, 29);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 4, 25, 12, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // btnSnooze
+            // 
+            this.btnSnooze.Enabled = false;
+            this.btnSnooze.Location = new System.Drawing.Point(133, 165);
+            this.btnSnooze.Name = "btnSnooze";
+            this.btnSnooze.Size = new System.Drawing.Size(137, 23);
+            this.btnSnooze.TabIndex = 2;
+            this.btnSnooze.Text = "Snooze";
+            this.btnSnooze.UseVisualStyleBackColor = true;
+            this.btnSnooze.Click += new System.EventHandler(this.btnSnooze_Click);
+            // 
+            // btnActivate
+            // 
+            this.btnActivate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnActivate.Location = new System.Drawing.Point(276, 130);
+            this.btnActivate.Name = "btnActivate";
+            this.btnActivate.Size = new System.Drawing.Size(68, 58);
+            this.btnActivate.TabIndex = 4;
+            this.btnActivate.Text = "Activate";
+            this.btnActivate.UseVisualStyleBackColor = false;
+            this.btnActivate.Click += new System.EventHandler(this.btnActivate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 219);
+            this.Controls.Add(this.btnActivate);
+            this.Controls.Add(this.btnSnooze);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Alarm Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnSnooze;
+        private System.Windows.Forms.Button btnActivate;
+    }
+}
+
---
+++ Alarm Clock/Alarm Clock/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alarm_Clock
+{
+    public partial class Form1 : Form
+    {
+        Boolean activate = false;
+        Boolean AlarmActive = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString("h:mm tt");
+            DateTime currenttime = DateTime.Now;
+            DateTime usertime = dateTimePicker1.Value;
+          
+            if (currenttime.Hour  == usertime.Hour && currenttime.Minute == usertime.Minute)
+            {
+                SoundPlayer alarm = new SoundPlayer();
+                try
+                {
+                    if (activate == true)
+                    {
+                        alarm.SoundLocation = @"\alarm.wav";
+                        alarm.Play();
+                        System.Threading.Thread.Sleep(1000);
+                        AlarmActive = true;
+                        btnSnooze.Enabled = true;
+                    }
+                    else
+                    {
+                        alarm.Stop();
+                        AlarmActive = false;
+                        btnSnooze.Enabled = false;
+                    }
+                    
+                    
+                }catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnActivate_Click(object sender, EventArgs e)
+        {
+            if (activate == true)
+            {
+                btnActivate.Text = "Activate";
+                activate = false;
+                btnActivate.BackColor = Color.Green;
+            }
+            else
+            {
+                btnActivate.Text = "Deactivate";
+                activate = true;
+                btnActivate.BackColor = Color.Red;
+            }
+        }
+
+
+        private void btnSnooze_Click(object sender, EventArgs e)
+        {
+           if (AlarmActive == true)
+            {
+                dateTimePicker1.Value = DateTime.Now.AddMinutes(10);
+                btnSnooze.Enabled = false;
+            }
+        }
+    }
+}
